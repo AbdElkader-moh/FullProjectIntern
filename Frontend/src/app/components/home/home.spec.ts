887eab471735d51105a770ad6423ff5c9495119b
ShahdAboelmakaram <157031485+shahdAboelmakarm22@users.noreply.github.com>
2026-05-02 19:16:19 +0300

    update unit test for frontend

--- Frontend/src/app/components/home/home.spec.ts
+++ Frontend/src/app/components/home/home.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
 
 import { Home } from './home';
 
 describe('Home', () => {
   let component: Home;
   let fixture: ComponentFixture<Home>;
+  let mockAuthService: any;
 
   beforeEach(async () => {
+    mockAuthService = {
+      currentUser: { firstName: 'Test', lastName: 'User' },
+      getProfile: () => of({ firstName: 'Test', lastName: 'User' }),
+      logout: () => of({ message: 'Logged out' })
+    };
+
     await TestBed.configureTestingModule({
       imports: [Home],
+      providers: [
+        provideRouter([{ path: 'signin', component: class {} }]),
+        { provide: AuthService, useValue: mockAuthService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
--- Frontend/src/app/components/signin/signin.spec.ts
+++ Frontend/src/app/components/signin/signin.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { Signin } from './signin';
 
@@ -9,6 +12,11 @@ describe('Signin', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Signin],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Signin);
