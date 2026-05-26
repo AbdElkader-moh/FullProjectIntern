2e9560c41e1901e382954188d97424ea6f7f179c
ShahdAboelmakaram <157031485+shahdAboelmakarm22@users.noreply.github.com>
2026-05-26 20:59:50 +0300

    rest of configuration of jenkins

---
+++ jenkins-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Fix Docker socket permissions on every container start
+# This ensures Jenkins can always access Docker regardless of host socket ownership
+if [ -S /var/run/docker.sock ]; then
+    chmod 666 /var/run/docker.sock
+fi
+
+# Hand off to the official Jenkins entrypoint
+exec /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
